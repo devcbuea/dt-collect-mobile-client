e69a6deac6dc96acf98ab94b11da53abd767a672
bosz <fongohmartin@gmail.com>
2019-01-27 15:20:56 +0100

    Render splashscreen component in App component

--- mobile_client/index.js
+++ mobile_client/index.js
@@ -1,5 +1,4 @@
 import { AppRegistry } from 'react-native';
 import App from './App';
-import SplashScreen from './src/screens/SplashScreen';
 
-AppRegistry.registerComponent('mobile_client', () => SplashScreen);
+AppRegistry.registerComponent('mobile_client', () => App);
