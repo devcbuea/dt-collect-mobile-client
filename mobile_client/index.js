eef00c9adc6e4dbe1e217ee6743df2d913cffbc5
bosz <fongohmartin@gmail.com>
2019-01-27 15:10:42 +0100

    Implemented splash screen, 3rd design from figma

--- mobile_client/index.js
+++ mobile_client/index.js
@@ -1,4 +1,5 @@
 import { AppRegistry } from 'react-native';
 import App from './App';
+import SplashScreen from './src/screens/SplashScreen';
 
-AppRegistry.registerComponent('mobile_client', () => App);
+AppRegistry.registerComponent('mobile_client', () => SplashScreen);
---
+++ mobile_client/src/screens/SplashScreen.js
@@ -0,0 +1,63 @@
+import React, {Component} from 'react'; 
+import {StyleSheet, ScrollView, View, Text} from 'react-native'; 
+
+export default class SplashScreen extends Component {
+	constructor(props) {
+	  	super(props);
+	
+	  	this.state = {};
+	}
+
+	componentDidMount() {
+
+	}
+
+	render() {
+		const {headerStyle, footerStyle, rootStyle, headerH1Style, headerH2Style, textColorStyle} = styles;
+		return (
+			<View style={ headerStyle }>
+				<ScrollView contentContainerStyle={rootStyle}>
+					<View>
+						<Text style={ [headerH1Style, textColorStyle] }>DevCBuea</Text>
+					</View>
+					<View>
+						<Text style={ [headerH2Style, textColorStyle] }>Data Collection Tool</Text>
+					</View>
+				</ScrollView>
+				<View style={ footerStyle }>
+					<Text style={[textColorStyle]}>Copyright (c) 2019</Text>
+				</View>
+			</View>
+		);
+	}
+}
+
+const styles = StyleSheet.create({
+	headerStyle: {
+		flex: 1, 
+		backgroundColor: '#4885ed',
+	}, 
+	footerStyle: {
+		marginTop: 10, 
+		marginBottom: 10,
+		alignItems: 'center',
+	    justifyContent: 'center',
+		// backgroundColor: '#fff', 
+	}, 
+	rootStyle: {
+		flexGrow : 1, 
+		justifyContent : 'center', 
+		alignItems: 'center' 
+	}, 
+	headerH1Style: {
+		fontSize: 40, 
+		fontWeight: 'bold', 
+	}, 
+	headerH2Style: {
+		fontSize: 16,
+		marginTop: 5, 
+	}, 
+	textColorStyle: {
+		color: '#fff',
+	}
+});
